a7850a5a112cdc1a96cb474144edebdf33f25608
warlleyfdz <warlleyfdz@hotmail.com>
2023-04-06 21:03:25 -0300

    primeiro commit

---
+++ src/main/java/com/primeirotranpodev/objetoSupermercado/ObjetoSupermercadoApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotranpodev.objetoSupermercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetoSupermercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetoSupermercadoApplication.class, args);
+	}
+
+}
